489f39c61124d39e11a3b494c13d5b902455c693
Nor Nikita <nor.nikit@yandex.ru>
2020-02-12 19:37:00 +0300

    add maven dependency for testing. begin write test

---
+++ src/test/java/har/task/com/junit/controller/HarFileLoaderTest.java
@@ -0,0 +1,9 @@
+package har.task.com.junit.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HarFileLoaderTest {
+
+}
