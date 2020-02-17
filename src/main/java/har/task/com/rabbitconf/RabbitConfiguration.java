152ea70e079a6ce33af73fd8b044007f5592a6f3
Nor Nikita <nor.nikit@yandex.ru>
2020-02-17 19:54:49 +0300

    refactoring: add params for configuration rabbitmq in application.yml

--- src/main/java/har/task/com/rabbitconf/RabbitConfiguration.java
+++ src/main/java/har/task/com/rabbitconf/RabbitConfiguration.java
@@ -6,15 +6,22 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfiguration {
 
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.queueName}")
+    private String queueName;
+
     @Bean
     public ConnectionFactory connectionFactory() {
-        return new CachingConnectionFactory("localhost");
+        return new CachingConnectionFactory(host);
     }
 
     @Bean
@@ -29,7 +36,7 @@ public class RabbitConfiguration {
 
     @Bean
     public Queue queue() {
-        return new Queue("harQueue");
+        return new Queue(queueName);
     }
 
  }
