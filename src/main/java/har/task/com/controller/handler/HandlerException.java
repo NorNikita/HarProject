92a60c6c82effeaf1781212ccb6b4247edae6f78
Nor Nikita <nor.nikit@yandex.ru>
2020-02-19 17:48:42 +0300

    refactoring HandlerException

--- src/main/java/har/task/com/controller/handler/HandlerException.java
+++ src/main/java/har/task/com/controller/handler/HandlerException.java
@@ -7,22 +7,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
 
 @ControllerAdvice(basePackages = "har.task.com.controller")
 public class HandlerException {
 
-    @ResponseBody
     @ExceptionHandler(HarFileNotFoundException.class)
     public ResponseEntity<ExceptionFormat> notFound(HarFileNotFoundException exc) {
         return new ResponseEntity<>(new ExceptionFormat(exc.getMessage()), HttpStatus.NOT_FOUND);
     }
 
-    @ResponseBody
-    @ExceptionHandler(IOException.class)
-    public ResponseEntity<ExceptionFormat> internalError(IOException exc) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionFormat> internalError(Exception exc) {
         return new ResponseEntity<>(new ExceptionFormat(exc.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
