89fd4d5ff5b4283460074fa938be5b799f747a0d
Nor Nikita <nor.nikit@yandex.ru>
2020-02-26 15:57:24 +0300

    refactoring end2end test (remove unused imports)

--- src/test/java/com/pflb/hartask/end2end/controller/HarControllerTest.java
+++ src/test/java/com/pflb/hartask/end2end/controller/HarControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
